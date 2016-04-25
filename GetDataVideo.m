7515fe59ed3988137df10e7035031f354f87e10c
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-04-25 09:36:13 -0400

    Test version of GetDataVideo.m

--- GetDataVideo.m
+++ GetDataVideo.m
@@ -4,7 +4,10 @@ function [Report] = GetDataVideo
 % savePath = '/Users/giulia/Desktop/TEST';
 % TRnum = Inf OR num of TRs
 
+
 %% set saving path and names
+clc
+
 formatOut = 'mmddyy_HHMMSS';
 timestamp = datestr((datetime('now')),formatOut);
 vidName = ['LiveTrackVIDEO_' timestamp];
@@ -29,7 +32,6 @@ diskLogger = VideoWriter(fullfile('/Users/giulia/Desktop/TEST/',vidName), 'Motio
 vid.DiskLogger = diskLogger;
 % set manual trigger
 triggerconfig(vid, 'manual') %change to appropriate trigger configuration
-
 preview(vid);
 [reports] = [0];
 LiveTrackHIDcomm(deviceNumber,'end');
@@ -37,9 +39,11 @@ PsychHID('SetReport', deviceNumber,2,0,uint8([103 zeros(1,63)]));
 LiveTrackHIDcomm(deviceNumber,'begin');
 start(vid); %initialize video ob
 firstTTL = true;
-stopTime = Inf;
+recTime = 60; %in seconds
 ii = 1;
 log = true;
+TimerFlag = false;
+
 fprintf('\n LiveTrack: Listening...');
 
 %% logging
@@ -57,12 +61,31 @@ while log
         trigger(vid);
         firstTTL = false;
         fprintf('\n TTL detected! \n');
-        stopTime = datenum(clock + [0, 0, 0, 0, 0, 20]);
+        %start timer
+        TimerFlag = true;
     end
-    if datenum(clock) > stopTime
-         log = false;
+    if TimerFlag == true
+        t = timer('TimerFcn',@timerFun,'StartDelay',recTime);
+        t.UserData = true;
+        start(t);
+        while get(t,'UserData')
+            display('LiveTrack: recording...');
+            pause(1);
+            PsychHID('ReceiveReports',deviceNumber);
+            [reports]=PsychHID('GiveMeReports',deviceNumber);
+            buffer = [buffer reports];
+            R = HID2struct(buffer);
+            Report = R;
+            R = 0;
+            [reports] = [0];
+            ii = ii+1;
+        end
+        log = t.UserData;
+        stop(t);
+        delete(t);
     end
 end
+
 LiveTrackHIDcomm(deviceNumber,'end');
 stop(vid);
 stoppreview(vid);
@@ -72,4 +95,14 @@ stoppreview(vid);
 closepreview(vid);
 save((fullfile('/Users/giulia/Desktop/TEST/',reportName)), 'Report');
 fprintf('Matfile and video saved.\n');
+
+
+
+
+
+function timerFun(obj,evt)
+display('LiveTrack:stopping...');
+log = false;
+set(obj,'UserData',false);
+
 
