0fa566905f913aa691b053fd2f4663a08c1cb046
Giulia Frazzetta <MELA@hntvlan554.385.wlan.wireless-pennnet.upenn.edu>
2016-05-31 11:36:12 -0400

    Calibration and Standard drivers update

--- ExperimentalProgram/code/drivers/LiveTrackfMRI_ScaleCalibrationDriver.m
+++ ExperimentalProgram/code/drivers/LiveTrackfMRI_ScaleCalibrationDriver.m
@@ -1,61 +1,89 @@
 function params = LiveTrackfMRI_ScaleCalibrationDriver(expt)
-% params = LiveTrackfMRI_ScaleCalibrationDriver(expt)
-%
-%
-%
-% For now, we just assign params to be the empty matrix.
+% This function is used to calibrate the size of the pupil tracked with a
+% CRS LiveTrackAV unit. It needs to be run after each session, pointing the
+% camera towards at least 3 different diameters on the calibration stick,
+% using the same focal distance used during the experiment (i.e. the
+% calibration stick must be placed so that the black dots appear in focus
+% as the subject eye did during the experiment).
+
+% HOW TO USE:
+% At the end of the session:
+% - remove the LiveTrackAV from the head mount, making sure not to screw in or
+% out the lens.
+% - place the calibration stick to a flat surface.
+% - point the camera towards the calibration stick, approximately 10-12
+% cm apart.
+% - run this function.
+% - select at least 3 diameters as reference for the calibration.
+% - adjust the camera/calibration stick position for every calibration dot
+% so that they appear in focus (DO NOT ADJUST THE FOCUS USING THE LENS
+% SCREW)
+% - collect the calibration videos (suggested length: 10 seconds)
+
+
+% May 2016 - Giulia Frazzetta, Manuel Spitschan: written  
+
+%% Set variables
+% Pre allocate params
 params = [];
 
 % Enter the scale diameters
 params.pupilDiameterMmGroundTruth = GetWithDefault('Enter pupil diameters on calibration stick you want to test. Use square brackets for multiple ones.', 5);
 
+% Get recTime
 recTime = expt.recTimeInSecs;
 
-%% find  Livetrack
-% data collection
+%% find  and set Livetrack
+% locate LiveTrack
 [deviceNumber, type] = crsLiveTrackGetHIDdeviceNumber;
-% video recording
+
+%% video recording for every sample diameter
 
 for ii = 1:length(params.pupilDiameterMmGroundTruth)
+    
+    % video name setting
     vidName = fullfile(expt.subjectDataDir, [expt.obsIDAndRun '-scaleCalibration_' num2str(params.pupilDiameterMmGroundTruth(ii)) 'Mm.avi']);
-    fprintf('>> Set up camera to show target of <strong>%g mm </strong>...', params.pupilDiameterMmGroundTruth(ii));
-    % video recording
+    
+    % locate video source (LiveTrack webcam interface)
     vid = videoinput('macvideo', 1, 'YUY2_320x240'); %'YCbCr422_1280x720') %;
     src = getselectedsource(vid);
     
-    %% find and set camera for video recording
-    fprintf('\nPress spacebar to initialize LiveTrack... ');
+    % prompt the user to adjust the camera over the correct target and
+    % initialize LiveTrack
+    fprintf('>> Set up camera to show target of <strong>%g mm </strong>...', params.pupilDiameterMmGroundTruth(ii));
+    fprintf('\nPress spacebar to initialize LiveTrack. The preview window will appear. ');
     pause;
-    
     
     %video recording settings
     vid.FramesPerTrigger = Inf;
-    frameRate = 30; %default fps
-
-    
     diskLogger = VideoWriter(vidName, 'Motion JPEG AVI');
+    diskLogger.FrameRate = 10;  % Note that this is the default LiveTrack Camera interface frameRate.
+    diskLogger.Quality = 75;
     vid.DiskLogger = diskLogger;
     vid.LoggingMode = 'disk';
-    
-        
-    % set manual trigger
     triggerconfig(vid, 'manual')
-    preview(vid);
-    start(vid); %initialize video ob
     
+    % verify that tracking is good with the preview window
+    preview(vid);
+    fprintf('\nMake sure that the calibration dot is correctly tracked, then press spacebar to close preview.');
+    pause
+    stoppreview(vid);
+    closepreview(vid);
+    
+    % initialize video object for recoding
+    start(vid);
+    
+    % initialize data acquisition
     [reports] = [0];
     PsychHID('SetReport', deviceNumber,2,0,uint8([103 zeros(1,63)]));
     
-    
-    fprintf('done!');
     fprintf('\n Press spacebar to start recording.');
     pause;
     
-    %% acquisition
-    %persistent buffer
+    % initialize the buffer
     buffer = [];
     
-    
+    % record calibration video and data
     if recTime == Inf
         trigger(vid);
         LiveTrackHIDcomm(deviceNumber,'begin');
@@ -71,10 +99,11 @@ for ii = 1:length(params.pupilDiameterMmGroundTruth)
     else
         trigger(vid);
         LiveTrackHIDcomm(deviceNumber,'begin');
+        display('LiveTrack: recording...');
         tic
-        while toc < recTime * 1.5 %buffer required to record the exact amount of seconds
-            display('LiveTrack: recording...');
+        while toc < recTime
             pause(1);
+            toc
             PsychHID('ReceiveReports',deviceNumber);
             [reports]=PsychHID('GiveMeReports',deviceNumber);
             buffer = [buffer reports];
@@ -85,19 +114,19 @@ for ii = 1:length(params.pupilDiameterMmGroundTruth)
         end
     end
     
+    % end data and video recording
     LiveTrackHIDcomm(deviceNumber,'end');
     pause(0.5);
     stop(vid);
-    stoppreview(vid);
-closepreview(vid);
+    
     
     % Make a copy of the report
     params.ReportRaw{ii} = Report;
     
-% cleanup
-delete(vid)
-close(gcf)
-
+    % clean the video object
+    delete(vid)
+    close(gcf)
+    
     
     % Calculate the conversion factors for width and height independently.
     params.cameraUnitsToMmWidth(ii) = mean([params.ReportRaw{ii}.LeftPupilWidth]) / params.pupilDiameterMmGroundTruth(ii);
@@ -107,5 +136,6 @@ end
 params.cameraUnitsToMmWidthMean = mean(params.cameraUnitsToMmWidth);
 params.cameraUnitsToMmHeightMean = mean(params.cameraUnitsToMmHeight);
 
+% display the conversion factors
 fprintf('>> Final conversion factor (camera units to mm - width): <strong>%.2f</strong>\n', params.cameraUnitsToMmWidthMean);
 fprintf('>> Final conversion factor (camera units to mm - height): <strong>%.2f</strong>\n', params.cameraUnitsToMmHeightMean);
--- ExperimentalProgram/code/drivers/LiveTrackfMRI_StandardDriver.m
+++ ExperimentalProgram/code/drivers/LiveTrackfMRI_StandardDriver.m
@@ -1,47 +1,79 @@
 function params = LiveTrackfMRI_StandardDriver(expt)
-% params = LiveTrackfMRI_StandardDriver(expt)
-%
-% Do the eye tracking here and assign to params
-%
-% For now, we just assign params to be the empty matrix.
+% This function is is our standard drive to do pupil tracking during fMRI
+% scans using a CRS LiveTrackAV unit. It will record an AVI video (10 fps)
+% and produce a MAT report with tracking values of the pupil. The report collection is initialized by the user. The video
+% recording is triggered by the first TR via a TTL input. Every TTL is
+% recorded in the report file. Video and data collection will end 5 seconds
+% later than the recording time set by the user.
+
+% HOW TO USE:
+% At the beginning of the session:
+% - make sure that the LiveTrackAV focusing screw has enough grip on the
+% thread.
+% - position the LiveTrackAV on the head mount.
+% - focus the lens on the subject pupil.
+% - verify the tracking on the preview window (it can be selected from
+% LiveTrackfMRI_RunExp.
+% - run this function.
+% - select the appropriate recording time in seconds
+% - initiate the LiveTrackAV
+
+% May 2016 - Giulia Frazzetta, Manuel Spitschan: written.
+
+%% Set variables
+% Pre allocate params
 params = [];
 
+%set video file name
 vidName = fullfile(expt.subjectDataDir, [expt.obsIDAndRun '.avi']);
 
-%% find  Livetrack
-% data collection
+% find  LivetrackAV for data collection
 [deviceNumber, type] = crsLiveTrackGetHIDdeviceNumber;
-LiveTrackHIDcomm(deviceNumber,'end'); %stop tracking
-% video recording
-vid = videoinput('macvideo', 1, 'YUY2_320x240'); %'YCbCr422_1280x720') %;
+
+% ind  LivetrackAV for video recording
+vid = videoinput('macvideo', 1, 'YUY2_320x240');
 src = getselectedsource(vid);
 
-%% initialize
+%% Data and video collection initialization
+% prompt user to initialize LiveTrackAV
 fprintf('\n Press spacebar to initialize LiveTrack.');
 pause;
-%video recording
+
+% video recording settings
 vid.FramesPerTrigger = Inf;
-frameRate = 30; %default fps
 vid.LoggingMode = 'disk';
 diskLogger = VideoWriter(vidName, 'Motion JPEG AVI');
+diskLogger.FrameRate = 10;  % Note that this is the default LiveTrack Camera interface frameRate.
+diskLogger.Quality = 75;
 vid.DiskLogger = diskLogger;
-% set manual trigger
 triggerconfig(vid, 'manual')
-preview(vid);
+
+% clear reports variable
 [reports] = [0];
+
+% reset the framecount
 PsychHID('SetReport', deviceNumber,2,0,uint8([103 zeros(1,63)]));
+
+% initiate data collection
 LiveTrackHIDcomm(deviceNumber,'begin');
-start(vid); %initialize video ob
+
+% initiate video object
+start(vid);
+
+% set starting flags
 firstTTL = true;
-ii = 1;
 log = true;
 TimerFlag = false;
 
+% notify listening mode (the camera is waiting for a TTL input)
 fprintf('\n LiveTrack: Listening...');
 
-%% logging
+%% Data and video collection
+% preallocate the buffer
 buffer = [];
+% record video and data
 while log
+    % wait for first TTL
     PsychHID('ReceiveReports',deviceNumber);
     [reports]=PsychHID('GiveMeReports',deviceNumber);
     buffer = [buffer reports];
@@ -49,19 +81,24 @@ while log
     Report = R;
     R = 0;
     [reports] = [0];
-    ii = ii+1;
+    
+    % detect first TTL
     if Report(end).Digital_IO1 == 1 && firstTTL
         trigger(vid);
         firstTTL = false;
         fprintf('\n TTL detected! \n');
+        
         %start timer
         TimerFlag = true;
+        fprintf('\n LiveTrack: recording...');
     end
+    
+    % record video after first TTL
     if TimerFlag == true
         tic
-        while toc < expt.recTimeInSecs * 1.1 %buffer required to record the exact amount of seconds
-            display('LiveTrack: recording...');
+        while toc < expt.recTimeInSecs + 5 %we record some extra seconds here.
             pause(1);
+            toc % elapsed time is displayed
             PsychHID('ReceiveReports',deviceNumber);
             [reports]=PsychHID('GiveMeReports',deviceNumber);
             buffer = [buffer reports];
@@ -69,7 +106,6 @@ while log
             Report = R;
             R = 0;
             [reports] = [0];
-            ii = ii+1;
         end
         display('LiveTrack:stopping...');
         log = false;
@@ -79,11 +115,10 @@ end
 LiveTrackHIDcomm(deviceNumber,'end');
 pause(0.5);
 stop(vid);
-stoppreview(vid);
-closepreview(vid);
 
+% save report
 params.Report = Report;
 
-% cleanup
+% clean the video object
 delete(vid)
 close(gcf)
