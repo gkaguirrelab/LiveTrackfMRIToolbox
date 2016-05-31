15f5541e65a9dcca3ce0765f1641e62851780ef7
Manuel Spitschan <mspits@sas.upenn.edu>
2016-05-31 13:50:39 -0400

    A little bit of code clean up
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- ExperimentalProgram/code/drivers/LiveTrackfMRI_StandardDriver.m
+++ ExperimentalProgram/code/drivers/LiveTrackfMRI_StandardDriver.m
@@ -1,11 +1,14 @@
 function params = LiveTrackfMRI_StandardDriver(expt)
+% params = LiveTrackfMRI_StandardDriver(expt)
+%
 % This function is is our standard drive to do pupil tracking during fMRI
 % scans using a CRS LiveTrackAV unit. It will record an AVI video (10 fps)
-% and produce a MAT report with tracking values of the pupil. The report collection is initialized by the user. The video
-% recording is triggered by the first TR via a TTL input. Every TTL is
-% recorded in the report file. Video and data collection will end 5 seconds
-% later than the recording time set by the user.
-
+% and produce a MAT report with tracking values of the pupil. The report
+% collection is initialized by the user. The video recording is triggered
+% by the first TR via a TTL input. Every TTL is recorded in the report
+% file. Video and data collection will end 5 seconds later than the
+% recording time set by the user.
+%
 % HOW TO USE:
 % At the beginning of the session:
 % - make sure that the LiveTrackAV focusing screw has enough grip on the
@@ -13,7 +16,7 @@ function params = LiveTrackfMRI_StandardDriver(expt)
 % - position the LiveTrackAV on the head mount.
 % - focus the lens on the subject pupil.
 % - verify the tracking on the preview window (it can be selected from
-% LiveTrackfMRI_RunExp.
+% LiveTrackfMRI_RunExpt.
 % - run this function.
 % - select the appropriate recording time in seconds
 % - initiate the LiveTrackAV
@@ -35,11 +38,11 @@ vid = videoinput('macvideo', 1, 'YUY2_320x240');
 src = getselectedsource(vid);
 
 %% Data and video collection initialization
-% prompt user to initialize LiveTrackAV
+% Prompt user to initialize LiveTrackAV
 fprintf('\n Press spacebar to initialize LiveTrack.');
 pause;
 
-% video recording settings
+% Video recording settings
 vid.FramesPerTrigger = Inf;
 vid.LoggingMode = 'disk';
 diskLogger = VideoWriter(vidName, 'Motion JPEG AVI');
@@ -48,24 +51,26 @@ diskLogger.Quality = 75;
 vid.DiskLogger = diskLogger;
 triggerconfig(vid, 'manual')
 
-% clear reports variable
+postBufferTime = 5; % 5 seconds
+
+% Clear reports variable
 [reports] = [0];
 
-% reset the framecount
-PsychHID('SetReport', deviceNumber,2,0,uint8([103 zeros(1,63)]));
+% Reset the framecount
+PsychHID('SetReport', deviceNumber, 2, 0, uint8([103 zeros(1,63)]));
 
-% initiate data collection
-LiveTrackHIDcomm(deviceNumber,'begin');
+% Initiate data collection
+LiveTrackHIDcomm(deviceNumber, 'begin');
 
-% initiate video object
+% Initiate video object
 start(vid);
 
-% set starting flags
+% Set starting flags
 firstTTL = true;
 log = true;
 TimerFlag = false;
 
-% notify listening mode (the camera is waiting for a TTL input)
+% Notify listening mode (the camera is waiting for a TTL input)
 fprintf('\n LiveTrack: Listening...');
 
 % Play a sound
@@ -74,38 +79,38 @@ y = sin(440*2*pi*t);
 sound(y, 20000);
 
 %% Data and video collection
-% preallocate the buffer
+% Preallocate the buffer
 buffer = [];
-% record video and data
+% Record video and data
 while log
-    % wait for first TTL
-    PsychHID('ReceiveReports',deviceNumber);
-    [reports]=PsychHID('GiveMeReports',deviceNumber);
+    % Wait for first TTL
+    PsychHID('ReceiveReports', deviceNumber);
+    [reports]=PsychHID('GiveMeReports', deviceNumber);
     buffer = [buffer reports];
     R = HID2struct(buffer);
     Report = R;
     R = 0;
     [reports] = [0];
     
-    % detect first TTL
+    % Detect first TTL
     if Report(end).Digital_IO1 == 1 && firstTTL
         trigger(vid);
         firstTTL = false;
         fprintf('\n TTL detected! \n');
         
-        %start timer
+        % Start timer
         TimerFlag = true;
         fprintf('\n LiveTrack: recording...');
     end
     
-    % record video after first TTL
+    % Record video after first TTL
     if TimerFlag == true
         tic
-        while toc < expt.recTimeInSecs + 5 %we record some extra seconds here.
+        while toc < expt.recTimeInSecs + bufferTime % We record some extra seconds here.
             pause(1);
-            toc % elapsed time is displayed
-            PsychHID('ReceiveReports',deviceNumber);
-            [reports]=PsychHID('GiveMeReports',deviceNumber);
+            toc % Elapsed time is displayed
+            PsychHID('ReceiveReports', deviceNumber);
+            [reports]=PsychHID('GiveMeReports',d eviceNumber);
             buffer = [buffer reports];
             R = HID2struct(buffer);
             Report = R;
@@ -116,14 +121,14 @@ while log
         log = false;
     end
 end
-% stop video e data recording
-LiveTrackHIDcomm(deviceNumber,'end');
+% Stop video and data recording
+LiveTrackHIDcomm(deviceNumber, 'end');
 pause(0.5);
 stop(vid);
 
-% save report
+% Save report
 params.Report = Report;
 
-% clean the video object
+% Clean the video object
 delete(vid)
 close(gcf)
