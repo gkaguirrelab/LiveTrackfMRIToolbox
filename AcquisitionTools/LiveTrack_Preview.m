ea9f91fb6ca9778b4d8f4809a2cb36769f5ada77
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-09-01 15:00:36 -0400

    added \n

--- AcquisitionTools/LiveTrack_Preview.m
+++ AcquisitionTools/LiveTrack_Preview.m
@@ -8,7 +8,7 @@ vid = videoinput('macvideo', 1, 'YUY2_320x240'); %'YCbCr422_1280x720') %;
 fprintf('\n Press spacebar to open the preview window.');
 pause;
 preview(vid);
-fprintf('\n Press spacebar to end LiveTrack_preview.');
+fprintf('\n Press spacebar to end LiveTrack_preview.\n');
 pause;
 stoppreview(vid);
 closepreview(vid);
