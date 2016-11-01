3543f1a16cda0bbda5537ed1243deb1a48a31abe
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-11-01 10:55:56 -0400

    [DOC] improved comments for low level commands.

--- AcquisitionTools/closeLiveTrack.m
+++ AcquisitionTools/closeLiveTrack.m
@@ -1,7 +1,7 @@
 function closeLiveTrack
 
 % Function to safely stop the LiveTrack device. Using this before/instead
-% of "clear all" prevents PsychHID-related matlab crashes.
+% of "clear all" should prevent PsychHID-related matlab crashes.
 
 
 %% find LiveTrack
@@ -9,9 +9,11 @@ deviceList = PsychHID('Devices');
 productNames = arrayfun(@(x)x.product, deviceList,'UniformOutput',0);
 productName = strfind(productNames, 'LiveTrack');
 deviceNumber = find(not(cellfun('isempty', productName)));
-%% start and the stop livetrack
+%% start and then stop livetrack
+% low level command to start raw tracking (in camera pixel units) and return results (ref Livetrack user manual p.25)
 PsychHID('SetReport', deviceNumber, 2, 0, uint8([104 zeros(1,63)]));
 PsychHID('ReceiveReports', deviceNumber);
 [reports] = PsychHID('GiveMeReports', deviceNumber);
 PsychHID('ReceiveReportsStop',deviceNumber);
+% low level command to stop raw tracking (in camera pixel units) and return results (ref Livetrack user manual p.25)
 PsychHID('SetReport', deviceNumber, 2, 0, uint8([102 zeros(1,63)]));
