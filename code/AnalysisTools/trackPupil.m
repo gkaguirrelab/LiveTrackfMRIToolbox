f03fe7ecf4670db55b8d52bf902adfadddbb8130
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-03-31 13:52:56 -0400

    fix for backwards compatibility

--- code/AnalysisTools/trackPupil.m
+++ code/AnalysisTools/trackPupil.m
@@ -264,7 +264,7 @@ switch params.pupilFit
         pupil.X = nan(numFrames,1);
         pupil.Y = nan(numFrames,1);
         pupil.size = nan(numFrames,1);
-        pupil.bestCut = strings(numFrames,1);
+        pupil.bestCut = string(nan(numFrames,1));
         pupil.bestError = nan(numFrames,1);
         pupil.fullPerimeterLength = nan(numFrames,1);
         pupil.area = nan(numFrames,1);
