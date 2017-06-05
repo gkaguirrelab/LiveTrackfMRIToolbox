1c8aa3ce6267c71876314d20b201de5166bb53ec
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-06-05 13:55:19 -0400

    Added demo to extract perimeter

---
+++ code/AnalysisTools/DEMO_extractPerimeter.m
@@ -0,0 +1,133 @@
+%% clean up
+close all
+clear
+clc
+
+%% define the following
+[~, tmpName] = system('whoami');
+userName = strtrim(tmpName);
+
+dropboxDir = ['/Users/' userName '/Dropbox-Aguirre-Brainard-Lab'];
+dropboxDir = '/Volumes/Bay_2_data/giulia/Dropbox-Aguirre-Brainard-Lab';
+
+outDir = ['/Users/' userName '/Desktop'];
+
+%% sample run
+params.subjectName = 'TOME_3008';
+params.sessionDate = '102116';
+params.projectSubfolder = 'session1_restAndStructure';
+params.runName = 'rfMRI_REST_AP_run01';
+
+%% set paths
+videoPath = fullfile(dropboxDir,'TOME_processing',params.projectSubfolder,params.subjectName,params.sessionDate,'EyeTracking');
+params.inVideo = fullfile(videoPath,[params.runName '_60hz.avi']);
+params.outVideo = fullfile(outDir,[params.runName '_perimeter.avi']);
+
+
+
+%% tracking params
+% params for image resizing and cropping
+if ~isfield (params, 'keepOriginalSize')
+    params.keepOriginalSize = 0;
+end
+if ~isfield(params,'imageSize')
+    params.imageSize = [486 720]/2;
+end
+if ~isfield(params,'imageCrop')
+    params.imageCrop = [1 1 319 239];
+end
+
+% params for circleFit (always needed)
+    params.rangeAdjust = 0.05;
+    params.circleThresh = [0.085 0.999];
+    params.pupilRange   = [20 90];
+    params.glintRange   = [10 30];
+    params.glintOut     = 0.1;
+    params.sensitivity  = 0.99;
+    params.dilateGlint  = 5;
+    params.pupilOnly = 0;
+
+% structuring element for pupil mask size
+params.maskBox   = [4 30];
+sep = strel('rectangle',params.maskBox);
+
+% force number of frames        
+params.forceNumFrames = 200;
+params.ellipseThresh   = [0.9 0.9];
+params.gammaCorrection = 1;
+
+
+%% EXTRACTION OF THE PUPIL PERIMETER
+% Load video
+disp('Loading video file...');
+inObj                   = VideoReader(params.inVideo);
+numFrames               = floor(inObj.Duration*inObj.FrameRate);
+% option to overwrite numFrames (for quick testing)
+if isfield(params,'forceNumFrames')
+    numFrames = params.forceNumFrames;
+end
+
+% initialize gray image array
+grayI                   = zeros([240 320 numFrames],'uint8');
+
+disp('Converting video to standard format, may take a while...');
+% Convert to gray, resize, crop to livetrack size
+for i = 1:numFrames
+    thisFrame           = readFrame(inObj);
+    tmp                 = rgb2gray(thisFrame);
+    if params.keepOriginalSize == 0
+        tmp2 = imresize(tmp,params.imageSize);
+        tmp = imcrop(tmp2,params.imageCrop);
+    end
+    grayI(:,:,i) = tmp;
+end
+
+if isfield(params,'outVideo')
+    ih = figure;
+end
+if isfield(params,'outVideo')
+    outObj              = VideoWriter(params.outVideo);
+    outObj.FrameRate    = inObj.FrameRate;
+    open(outObj);
+end
+
+clear RGB inObj
+        
+% extract perimeter        
+for i = 1:numFrames
+    % Get the frame
+    I = squeeze(grayI(:,:,i));
+    
+    % adjust gamma for this frame
+    I = imadjust(I,[],[],params.gammaCorrection);
+    
+%     % Show the frame
+%     if isfield(params,'outVideo')
+%         imshow(I);
+%     end
+    
+    
+    % track with circles
+    pupilRange = params.pupilRange;
+    glintRange = params.glintRange;
+    [pCenters, pRadii,pMetric, gCenters, gRadii,gMetric, pupilRange, glintRange] = circleFit(I,params,pupilRange,glintRange);
+    % get pupil perimeter
+    [binP] = getPupilPerimeter(I,pCenters,pRadii, sep, params);
+    
+    imshow(binP)
+    
+    [Xp, Yp] = ind2sub(size(binP),find(binP));
+    
+    % save frame
+    if isfield(params,'outVideo')
+        frame   = getframe(ih);
+        writeVideo(outObj,frame);
+    end
+end
+
+
+% save video
+if isfield(params,'outVideo')
+    close(ih);
+    close(outObj);
+end
